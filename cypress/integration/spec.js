023f13b333a374351e48d677ac0157aa07c44383
Blake Mann <bmann@benevity.com>
2020-09-02 23:58:00 -0600

    Set up example test cases

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,26 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+  it('works with an external url using url', () => {
+    cy.route2({ url: 'https://example.cypress.io/example-path', method: 'GET' }, { fixture: 'example.json' }).as('example');
+    cy.visit('index.html');
+    cy.contains('Hello');
+  })
+
+  it('works with an external url using path', () => {
+    cy.route2({ path: '/example-path', method: 'GET' }, { fixture: 'example.json' }).as('example');
+    cy.visit('index.html');
+    cy.contains('Hello');
+  })
+
+  it('works with an external url using hostname', () => {
+    cy.route2({ hostname: 'example.cypress.io', method: 'GET' }, { fixture: 'example.json' }).as('example');
+    cy.visit('index.html');
+    cy.contains('Hello');
+  })
+
+  it('works with an external url using hostname and path', () => {
+    cy.route2({ hostname: 'example.cypress.io', path: '/example-path', method: 'GET' }, { fixture: 'example.json' }).as('example');
+    cy.visit('index.html');
+    cy.contains('Hello');
   })
 })
